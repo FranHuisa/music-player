33c9a44a20e89789ca342e06936132b50a8ea441
franciscohuisa9 <francisco.huisa9@gmail.com>
2026-05-13 10:57:23 +0200

    Sidebar solo para user

--- src/main/webapp/app/layouts/main/main.ts
+++ src/main/webapp/app/layouts/main/main.ts
@@ -46,6 +46,11 @@ export default class Main implements OnInit {
     if (!userAccount || isPublicPage) {
       return false;
     }
+
+    if (userAccount.authorities.includes(Authority.ADMIN)) {
+      return false;
+    }
+
     return userAccount.authorities.includes(Authority.USER);
   });
 
